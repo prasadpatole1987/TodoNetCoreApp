0a14310cda7e846759c8eeeb1a85d7d6402e8aa2
prasadpatole1987 <prasad.patole1987@gmail.com>
2020-04-10 13:54:39 +0530

    Added Sql db

---
+++ TodoApp.DB/dbo/Tables/TodoRequest.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TodoRequest] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Title]       NVARCHAR (50)  NOT NULL,
+    [Description] NVARCHAR (500) NOT NULL,
+    [IsComplete]  BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
