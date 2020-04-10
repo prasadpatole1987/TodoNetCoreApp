c11bcb75bcdf0271c92ab756213f5b3600a79156
prasadpatole1987 <prasad.patole1987@gmail.com>
2020-04-10 14:02:50 +0530

    Modified todorequest.title column size

--- TodoApp.DB/dbo/Tables/TodoRequest.sql
+++ TodoApp.DB/dbo/Tables/TodoRequest.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[TodoRequest] (
     [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [Title]       NVARCHAR (50)  NOT NULL,
+    [Title]       NVARCHAR (100)  NOT NULL,
     [Description] NVARCHAR (500) NOT NULL,
     [IsComplete]  BIT            NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
